fd91a042636667854f60a727e82a00af4adc1c27
Jennifer Hamon <jhamon@gmail.com>
2013-10-03 21:51:33 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdoptMeme::Application.config.secret_token = '16447d7a85f06e732062c877eff77a64506cc5cefb1bf0ac8f56380d6c3ae752716fbcf3780ee2bbcb3032f609c028d6c1150734ad4b115936b6036cf3dea886'
