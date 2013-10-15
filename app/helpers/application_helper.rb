134375a73def49b40aa3f8de3c4ec59781d3b009
Jennifer Hamon <jhamon@gmail.com>
2013-10-15 11:31:29 -0700

    Add several methods to ApplicationHelpers: login, logout, current_user, logged_in?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def logged_in?
+    !!self.current_user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_token(session[:seesion_token])
+  end
+  
+  def login(user)
+    session[:session_token] = user.reset_session_token
+    user.save
+  end
+  
+  def logout
+    session[:session_token] = nil
+    user.reset_session_token
+    user.save
+  end
 end
