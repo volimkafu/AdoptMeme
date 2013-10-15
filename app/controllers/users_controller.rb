7e6c887ceec2fce584d7075156a1c96be1c1cd65
Jennifer Hamon <jhamon@gmail.com>
2013-10-15 11:14:54 -0700

    Add basic UsersController logic.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
       render :index
     end
   end
+  
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    @user = User.create(params[:user])
+    if @user.save
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
 end
