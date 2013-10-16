a594c160a81d80679d59fdfa3257f8e36369d5a3
Jennifer Hamon <jhamon@gmail.com>
2013-10-16 15:05:36 -0700

    Change #find_by_credentials to accept user params hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,34 +4,38 @@ class User < ActiveRecord::Base
   [:email, :username].each do |attribute|
       validates attribute, :presence => true, :uniqueness => true
   end
-    
+
   validates :zipcode, :format => { :with => /^\d{5}$/ }
-  
+
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
   end
-  
+
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
-  
-  
+
+
   def self.find_by_token(token)
     User.where(:session_token => token).first
   end
-  
-  def self.find_by_credentials(username_or_email, password)
-    user = User.where(:email => username_or_email).first
-    return user if (!!user && user.is_password?(password))
-    user = User.where(:username => username_or_email).first
-    return user if (!!user && user.is_password?(password))  
+
+  def self.find_by_credentials(user_hash)
+    if user.keys.include?(:email)
+      user = User.where(:email => user[:email]).first
+      return user if (!!user && user.is_password?(password))
+    elsif user.keys.include?(:username)
+      user = User.where(:username => username_or_email).first
+      return user if (!!user && user.is_password?(password))
+    end
+
     nil
   end
-  
+
   def reset_session_token
     self.session_token = SecureRandom.urlsafe_base64(32)
   end
-  
+
   has_many(
     :images,
     :foreign_key => :uploader_id,
