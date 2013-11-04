8765d2ca0da6f907d55ce349d89a863b60a9922d
Jennifer Hamon <jhamon@gmail.com>
2013-11-03 17:56:04 -0800

    Create a pets index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AdoptMeme::Application.routes.draw do
   resources :images, :users
   resource :session, :only => [:new, :create, :destroy]
+  resources :pets, :only => [:index]
+  root :to => "pets#index"
 end
