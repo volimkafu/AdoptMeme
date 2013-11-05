f56b8d7a8721d8967e6004cfa5b63630a2ebada0
Jennifer Hamon <jhamon@gmail.com>
2013-11-04 21:09:03 -0800

    Starter pets controller.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,15 @@
 class PetsController < ApplicationController
   def index
+    @pets = Pet.limit(10)
+    render :index
+  end
+
+  def show
+    begin
+      @pet = Pet.find(params[:petid])
+      render :show
+    rescue
+      redirect_to '/'
+    end
   end
 end
