09a73cd5fc5ba2c090c3e55d2dac332c98d65c46
Jennifer Hamon <jhamon@gmail.com>
2013-11-04 21:02:07 -0800

    Add some /:petid routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 AdoptMeme::Application.routes.draw do
-  resources :images, :users
+  resources :users, :only => [:index, :new, :create]
   resource :session, :only => [:new, :create, :destroy]
-  resources :pets, :only => [:index]
   root :to => "pets#index"
+  get '/:petid', to: "pets#show"
+  get '/:petid/new', to: "captions#new"
+  get '/:petid/:captionid', to: "captions#show"
 end
