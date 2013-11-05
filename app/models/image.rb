bd55b27bc06531357e1844e98584b2c6363fa3dd
Jennifer Hamon <jhamon@gmail.com>
2013-11-05 11:19:02 -0800

    Images will be pulled from Petfinder.com, not user submitted.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,12 +2,7 @@ class Image < ActiveRecord::Base
   attr_accessible :uri, :uploader
   validates :uri, :presence => true
 
-  belongs_to(
-    :uploader,
-    :foreign_key => :uploader_id,
-    :primary_key => :id,
-    :class_name => "User"
-  )
+  belongs_to :pet
 
   has_many(
     :captions,
