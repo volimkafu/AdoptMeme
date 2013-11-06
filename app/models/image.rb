eb56b00c2cc86142567bc2cf870d69cabdd5e5dc
Jennifer Hamon <jhamon@gmail.com>
2013-11-06 14:27:26 -0800

    Reorganize code using AWS.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ActiveRecord::Base
-  validates :petfinder_url, :pet_id, :presence => true
+  validates :petfinder_url, :presence => true
 
   after_create :push_image_to_aws
-  after_destroy :delete_pets_without_images
+  # after_destroy :delete_pets_without_images
 
   belongs_to :pet
   has_many :captions, :inverse_of => :image
@@ -16,12 +16,13 @@ class Image < ActiveRecord::Base
   # handle_asynchronously :create_aws_object
 
   def push_image_to_aws
+    resource_name = self.aws_resource_name
+    self.amazon_aws_url = "http://s3.amazonaws.com/adoptmeme/" + resource_name
+    self.save
+
     begin
-      sleep 0.5 # slow down requests to petfinder.
+      sleep 0.25 # slow down requests to petfinder.
       image = RestClient.get(self.petfinder_url)
-      resource_name = self.aws_resource_name
-      self.amazon_aws_url = "http://s3.amazonaws.com/adoptmeme/" + resource_name
-      self.save
       create_aws_object(resource_name, image)
     rescue
       puts "There was a problem with Image #{self.id}"
@@ -29,17 +30,17 @@ class Image < ActiveRecord::Base
     end
   end
 
+  def aws_id
+    # Sequential ids are disfavored on S3 for performance reasons.
+    # So we reverse the id.
+    self.id.to_s.reverse
+  end
+
+  def aws_resource_name
+    "#{self.aws_id}.jpg"
+  end
+
   private
-    def aws_id
-      # Sequential ids are disfavored on S3 for performance reasons.
-      # So we reverse the id.
-      self.id.to_s.reverse
-    end
-
-    def aws_resource_name
-      "#{self.aws_id}.jpg"
-    end
-
     def delete_pets_without_images
       if pet.images.count.zero?
         pet.destroy
