6d93321fea5e69a76e4ca5a08b02e18ff9e642e9
Jennifer Hamon <jhamon@gmail.com>
2013-11-07 13:28:57 -0800

    Refactor caption.js text placement.

--- app/assets/javascripts/captions.js
+++ app/assets/javascripts/captions.js
@@ -15,23 +15,56 @@ $(document).ready( function () {
 
   function render() {
     context.drawImage(background,0,0);
-    var topText = $('#toptext').val()
-    var bottomText = $('#bottomtext').val();
+    var topText = $('#toptext').val().toUpperCase();
+    var bottomText = $('#bottomtext').val().toUpperCase();;
 
     var center_x = canvas.width / 2;
-    var top_y = 50;
     var bottom_y = canvas.height - 50;
 
     context.textAlign = "center";
     context.font = "bold 60px Impact, sans-serif";
+    context.lineWidth = 3;
+    context.fontsize = 75;
     context.fillStyle = "white";
     context.strokeStyle = "black";
-    context.lineWidth = 3;
-    context.fillText(topText, center_x, top_y);
-    context.strokeText(topText, center_x, top_y);
 
-    context.fillText(bottomText, center_x, bottom_y);
-    context.strokeText(bottomText, center_x, bottom_y);
+    // drawText(topText, center_x, top_y)
+    // drawText(bottomText, center_x, bottom_y)
+    drawTopText(topText, center_x, 65)
+
+    context.lineWidth = 2.5;
+    context.fontsize = 75;
+    drawBottomText(bottomText, center_x)
+  }
+
+  function drawText(text, xpos, ypos) {
+    var startText = text.slice(0,30)
+    context.font = "bold "+context.fontsize+"px Impact, sans-serif";
+    while (context.measureText(startText).width > (canvas.width-20)) {
+      ypos = ypos - 2;
+      context.fontsize = context.fontsize - 4;
+      context.lineWidth = context.lineWidth - 0.1;
+      context.font = "bold "+context.fontsize+"px Impact, sans-serif";
+    }
+
+    context.fillText(startText, xpos, ypos);
+    context.strokeText(startText, xpos, ypos);
+  }
+
+  function drawTopText(text, xpos, ypos) {
+    drawText(text.slice(0,30), xpos, ypos)
+    if (text.slice(30) !== "") {
+      drawTopText(text.slice(30), xpos, ypos+40);
+    }
+  }
+
+  function drawBottomText(text, xpos) {
+    var lines = Math.ceil(text.length/30);
+    ypos = canvas.height - lines*30;
+    for (var i=0; i < lines; i++) {
+      drawText(text.slice(i*30, (i+1)*30), xpos, ypos)
+      ypos += 40;
+    }
   }
 
   $('#toptext').on("keyup", function () {
