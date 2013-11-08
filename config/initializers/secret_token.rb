3272f7a0123db50ce6e985b29cf93ed74d5a1292
Jennifer Hamon <jhamon@gmail.com>
2013-11-07 18:44:27 -0800

    Remove secret token. Prefer setting this with figaro/environment variables.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AdoptMeme::Application.config.secret_token = '16447d7a85f06e732062c877eff77a64506cc5cefb1bf0ac8f56380d6c3ae752716fbcf3780ee2bbcb3032f609c028d6c1150734ad4b115936b6036cf3dea886'
+AdoptMeme::Application.config.secret_token = ENV["ADOPTMEME_SECRET_TOKEN"]
