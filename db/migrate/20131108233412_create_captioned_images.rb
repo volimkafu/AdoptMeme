4d8abe27d877ae725a446af7bc818f2ed67955a6
Jennifer Hamon <jhamon@gmail.com>
2013-11-08 20:22:49 -0800

    Remove extraneous files related to defuncted CaptionedImage model.

--- db/migrate/20131108233412_create_captioned_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateCaptionedImages < ActiveRecord::Migration
-  def change
-    create_table :captioned_images do |t|
-      t.integer :caption_id
-      t.string :aws_url
-      t.timestamps
-    end
-  end
-end
