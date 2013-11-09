3d2af8ef08fa467c3a5b98a8f265cb53d5426113
Jennifer Hamon <jhamon@gmail.com>
2013-11-08 20:22:02 -0800

    Refactor Caption model specs.

--- spec/models/caption_spec.rb
+++ spec/models/caption_spec.rb
@@ -1,40 +1,24 @@
 require 'spec_helper'
 
 describe Caption do
-  
+
   let (:captioner) { User.new(username: "sampleuser", email: "user@example.com") }
   let (:img) { Image.new(uri: 'fry.jpg')}
-  
+
   subject(:caption) do
     caption = Caption.new
     caption.captioner = captioner
     caption.image = img
     caption.top_text = "Not sure if trolling"
     caption.bottom_text = "or serious."
-    caption.save!  
+    caption.save!
     caption
   end
-    
-  it { should respond_to(:captioner) }
-  it { should respond_to(:image) }
-  it { should respond_to(:top_text) } 
+
+  it { should respond_to(:top_text) }
   it { should respond_to(:bottom_text) }
-  
-  describe "validations" do
-    it "should not allow the top and bottom text to both be blank" do
-      caption.top_text = ""
-      caption.bottom_text = ""
-      expect(caption).not_to be_valid
-    end
-    
-    it "must have a captioner" do
-      caption.captioner = nil
-      expect(caption).not_to be_valid
-    end
-    
-    it "must have a related image" do
-      caption.image = nil
-      expect(caption).not_to be_valid
-    end
-  end
+  it { should validate_presence_of(:image) }
+  it { should belong_to(:captioner) }
+  it { should belong_to(:image) }
+
 end
