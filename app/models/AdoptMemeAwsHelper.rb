9a900daa499da3044aadc082102f9a58bf6f7276
Jennifer Hamon <jhamon@gmail.com>
2013-11-09 00:01:32 -0800

    Add additional CRUD methods for aws bucket objects.

--- app/models/AdoptMemeAwsHelper.rb
+++ app/models/AdoptMemeAwsHelper.rb
@@ -1,6 +1,11 @@
 module AdoptMemeAwsHelper
+  # For details on the workings of aws-sdk, see the docs at
+  # http://docs.aws.amazon.com/AWSRubySDK/latest/frames.html
+
+  BUCKET_NAME = :adoptmeme
+
   def bucket
-    @bucket ||= s3.buckets[:adoptmeme]
+    @bucket ||= s3.buckets[BUCKET_NAME]
   end
 
   def s3
@@ -8,8 +13,8 @@ module AdoptMemeAwsHelper
   end
 
   def aws_id
-    # Sequential ids are disfavored on S3 for performance reasons.
-    # So we reverse the id.
+    # Sequential ids are disfavored on S3 for performance
+    # reasons. So we reverse the id.
     self.id.to_s.reverse
   end
 
@@ -17,13 +22,19 @@ module AdoptMemeAwsHelper
     "#{self.aws_id}.jpg"
   end
 
-  def create_aws_object(name, content)
-    # Creating objects: http://docs.aws.amazon.com/AWSRubySDK/latest/AWS/S3/S3Object.html
-    bucket.objects[name].write(content)
+  def create_aws_object(object_name, content)
+    bucket.objects[object_name].write(content)
   end
-  # handle_asynchronously :create_aws_object
+
+  def update_aws_object(object_name, content)
+    create_aws_object(object_name, content)
+  end
 
   def delete_aws_object(object_name)
-    bucket[object_name].delete
+    bucket.objects[object_name].delete
+  end
+
+  def get_aws_object(name)
+    bucket.objects[object_name].read
   end
 end
