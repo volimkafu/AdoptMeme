014829c213ca7872976bc5521e83d566993d5a2f
Jennifer Hamon <jhamon@gmail.com>
2013-11-08 17:39:15 -0800

    Add image manipulation functionality into Caption model.

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -1,8 +1,12 @@
-class Caption < ActiveRecord::Base
-  validates :captioner, :image, :presence => true
+require 'rmagick'
 
+class Caption < ActiveRecord::Base
+  include AdoptMemeAwsHelper
+  include Magick
+
+  validates :captioner, :image, :presence => true
   belongs_to :image
-  has_many :captioned_images, :inverse_of :caption
+  belongs_to :captioner
 
   belongs_to(
     :captioner,
@@ -10,5 +14,27 @@ class Caption < ActiveRecord::Base
     :primary_key => :id,
     :class_name => "User"
   )
+
+  def source
+    @image_src ||= RestClient.get(self.image.amazon_aws_url)
+    @source ||= Image.read(@image_src).first
+  end
+
+  def fontsize(message)
+    self.source.columns / message.length - 5
+  end
+
+  def draw_top_text(message)
+    Draw.new.annotate(@source,0,0,0,10, message) {
+        self.font_family = 'Impact'
+        self.fill = 'white'
+        self.stroke = 'black'
+        self.stroke_width = 3
+        #self.pointsize = 32
+        self.pointsize = fontsize(source, message)
+        self.font_weight = Magick::BoldWeight
+        self.gravity = Magick::NorthGravity
+    }
+  end
 
 end
--- app/models/captioned_image.rb
+++
@@ -1,5 +0,0 @@
-class CaptionedImage < ActiveRecord::Base
-  include AdoptMemeAwsHelper
-  belongs_to :caption
-  validates :caption_id, :presence => true
-end
