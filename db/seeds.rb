ebf66e5d7b6ef9ceff63a1fe8a01548288d2d07a
Jennifer Hamon <jhamon@gmail.com>
2013-11-11 10:40:34 -0800

    Clear out old seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.transaction do 
-  User.create(fname: 'Jennifer', lname: 'Hamon', zipcode: '32444', email: 'jhamon@gmail.com', username: 'jen' )
-  User.create(fname: 'Jeff', lname: 'Fiddler', zipcode: '94303', email: 'jeff@gmail.com', username: 'jeff' )
-  User.create(fname: 'Renata', lname: 'Cummins', zipcode: '94303', email: 'rcummins@gmail.com', username: 'renata' )
-end
-
