44781597b4984068af3a96c93af559f3272ff205
Jennifer Hamon <jhamon@gmail.com>
2013-11-11 14:27:35 -0800

    Add StaticPagesController to serve backbone app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 AdoptMeme::Application.routes.draw do
   resources :users, :only => [:index, :new, :create]
   resource :session, :only => [:new, :create, :destroy]
-  root :to => "images#index"
+  root :to => "static_pages#root"
   resources :captions, :only => [:create, :index]
   get '/:captionid', to: "captions#show"
   get '/:imageid/new', to: "captions#new"
