8093379ac59a600294f38c6a7c5bff7ab103ab8e
Jen Hamon <jhamon@gmail.com>
2013-11-12 14:17:29 -0800

    Override parse method to create associated pet objects.

--- app/assets/javascripts/models/petImage.js
+++ app/assets/javascripts/models/petImage.js
@@ -1,1 +1,12 @@
-AdoptMeme.Models.petImage = Backbone.Model.extend({})
+AdoptMeme.Models.petImage = Backbone.Model.extend({
+	info: "model petImage",
+
+	parse: function (data, options) {
+		var pet = new AdoptMeme.Models.pet(data.pet);
+		debugger
+		AdoptMeme.pets.add(pet);
+		delete data.pet;
+		debugger
+		return data;
+	}
+})
