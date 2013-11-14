8bf209e3f80e8c47bdc1379e1d32a44fc54d857d
Jen Hamon <jhamon@gmail.com>
2013-11-14 14:44:38 -0800

    Add hovercard on cat index page.

--- app/assets/javascripts/views/petDetailView.js
+++ app/assets/javascripts/views/petDetailView.js
@@ -1,7 +1,18 @@
 AdoptMeme.Views.petDetailView = Backbone.View.extend({
   className: 'cat-tile',
-  
+
   template: JST["pets/cat-tile"],
+
+  events: {
+    "mouseenter": "toggleHoverCard",
+    "mouseleave": "toggleHoverCard"
+  },
+
+  toggleHoverCard: function (event) {
+    console.log(event.currentTarget)
+    console.log(event.target)
+    this.$el.find('.hovercard').toggle()
+  },
 
   render: function () {
     var that = this;
