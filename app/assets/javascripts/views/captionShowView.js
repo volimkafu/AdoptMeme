30ed358f4a9c1b2177ae699af71a335a0b2794e6
Jen Hamon <jhamon@gmail.com>
2013-11-14 11:41:40 -0800

    Avoid infinte loops when visiting a caption url that doesnt exist. e.g. localhost:3000/1000

--- app/assets/javascripts/views/captionShowView.js
+++ app/assets/javascripts/views/captionShowView.js
@@ -3,21 +3,24 @@ AdoptMeme.Views.captionShowView = Backbone.View.extend({
 
   template: JST['captions/show'],
 
+  initialize: function () {
+    this.listenTo(this.collection, "change sync", this.render);
+  },
+
   initialize: function (options) {
   	this.captionid = options.captionid;
   	this.listenTo(this.collection, "change sync", this.render);
+    this.collection.fetch()
   },
 
   render: function () {
   	var caption = this.collection.get(this.captionid)
-  	if (caption) {
-      debugger
-	    var renderedContent = this.template({ caption: caption.attributes })
-	    this.$el.html(renderedContent)
-	    return this
-  	} else {
-  		this.collection.fetch()
-  		return this
-  	}
+    
+    if (caption) {
+      var renderedContent = this.template({ caption: caption.attributes });
+      this.$el.html(renderedContent);
+    }
+
+		return this
   }
 })
