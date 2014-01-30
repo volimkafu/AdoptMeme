0ec244573d663a7eb562d430cc1f8186264cf492
Jen Hamon <jhamon@gmail.com>
2014-01-30 07:22:01 -0800

    JSLinted index page.

--- app/assets/javascripts/views/mixedIndexView.js
+++ app/assets/javascripts/views/mixedIndexView.js
@@ -8,41 +8,41 @@ AdoptMeme.Views.mixedIndexView = Backbone.View.extend({
 
   $el: $(".content-container"),
 
-  template: JST['pets/index'],
+  template: JST["pets/index"],
 
   events: {
     "click #get-started":"scrollToStart"
   },
 
-  scrollToStart: function (event) {
+  scrollToStart: function () {
     console.log("trigger");
-    $('html,body').animate({
+    $("html,body").animate({
         scrollTop: document.documentElement.scrollTop() + 100
-    })
+    });
   },
 
   render: function () {
-    var $tileContainer = $("<div class='tile-container clearfix'></div>")
-    var $col1 = $("<div class='column'></div>")
-    var $col2 = $("<div class='column'></div>")
-    var $col3 = $("<div class='column'></div>")
-    var $col4 = $("<div class='column'></div>")
+    var $tileContainer = $("<div class='tile-container clearfix'></div>");
+    var $col1 = $("<div class='column'></div>");
+    var $col2 = $("<div class='column'></div>");
+    var $col3 = $("<div class='column'></div>");
+    var $col4 = $("<div class='column'></div>");
 
     var that = this;
     var newDivs = [];
 
-    this.petImages.each( function (pet, idx) {
-      var petDetail = new AdoptMeme.Views.petDetailView({ model: pet })
+    this.petImages.each( function (pet) {
+      var petDetail = new AdoptMeme.Views.petDetailView({ model: pet });
       petDetail.render();
       petDetail.$el.addClass("greyscale");
       newDivs.push(petDetail.$el);
-    })
+    });
 
-    this.captions.each( function (pet, idx) {
-      var petDetail = new AdoptMeme.Views.captionDetailView({ model: pet })
+    this.captions.each( function (pet) {
+      var petDetail = new AdoptMeme.Views.captionDetailView({ model: pet });
       petDetail.render();
       newDivs.push(petDetail.$el);
-    })
+    });
 
     newDivs = _.shuffle(newDivs);
 
@@ -56,14 +56,15 @@ AdoptMeme.Views.mixedIndexView = Backbone.View.extend({
       } else if (idx % 4 === 3) {
         $col4.append(div);
       }
-    })
+    });
 
     $tileContainer.append($col1);
     $tileContainer.append($col2);
     $tileContainer.append($col3);
     $tileContainer.append($col4);
-    that.$el.html(JST['layouts/hero'])
+    that.$el.html(JST["layouts/hero"]);
+    that.$el.append(JST["layouts/topbar"]);
     that.$el.append($tileContainer);
-    return this
+    return this;
   }
-})
+});
