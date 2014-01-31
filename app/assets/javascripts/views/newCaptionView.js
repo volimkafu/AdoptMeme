318eb54ed9926f41b318d7211b3d96b91ed841a3
Jen Hamon <jhamon@gmail.com>
2014-01-30 19:08:39 -0800

    Complex word splitting to prevent really long words from breaking the editor.

--- app/assets/javascripts/views/newCaptionView.js
+++ app/assets/javascripts/views/newCaptionView.js
@@ -13,7 +13,8 @@ AdoptMeme.Views.newCaptionView = Backbone.View.extend({
 			pet: AdoptMeme.pets.get(this.model.attributes.pet_id),
 			image: this.model.attributes
 		});
-		this.$el.html(renderedContent);
+    this.$el.html(JST["layouts/topbar"]);
+		this.$el.append(renderedContent);
 		return this;
 	},
 
@@ -31,7 +32,7 @@ AdoptMeme.Views.newCaptionView = Backbone.View.extend({
 				that.uploadImage(caption);
 			},
 			error: function () {
-				console.log("Error: There was a problem uploading the image data.")
+				console.log("Error: There was a problem uploading the image data.");
 				AdoptMeme.Routers.router.navigate("/", {trigger: true});
 			}});
 	},
@@ -74,6 +75,7 @@ AdoptMeme.Views.newCaptionView = Backbone.View.extend({
 	configureEditor: function () {
 	  var canvas = this.$canvas = $("#catCanvas")[0];
 	  var context = this.$context = canvas.getContext("2d");
+    this.maxLineLength = 30;
 
 	  var background = this.background = new Image();
 	  background.src = "/api/proxy_images/" + this.model.attributes.id;
@@ -99,53 +101,136 @@ AdoptMeme.Views.newCaptionView = Backbone.View.extend({
     context.drawImage(this.background,0,0);
     context.lineJoin = "bevel";
 
-    var center_x = canvas.width / 2;
-    var bottom_y = canvas.height - 50;
+    var center_x = this.center_x = canvas.width / 2;
 
     context.textAlign = "center";
-    context.lineWidth = 3;
+    context.lineWidth = 5;
     context.fillStyle = "white";
     context.strokeStyle = "black";
 
-    this.drawTopText(topText, center_x, 65);
+    this.drawTopText(topText, center_x, 75);
 
     context.lineWidth = 2.5;
-    this.drawBottomText(bottomText, center_x);
+    this.drawSingleLineBottomText(bottomText, center_x);
   },
 
-  drawText: function (text, xpos, ypos) {
+  drawTopText: function (toptext, centerx) {
+    var lines = this.buildLines(toptext);
+    console.log(lines);
+    if (lines.length > 1) {
+      this.drawMultiLineTopText(lines);
+    } else {
+      this.drawSingleLineTopText(toptext, centerx);
+    }
+  },
+
+  configureTextSize: function (text) {
+    var context = this.$context;
+    var canvas = this.$canvas;
+    context.lineWidth = 4;
+    context.fontsize = 80;
+    context.font = "bold "+context.fontsize+"px Impact, sans-serif";
+
+    while (context.measureText(text).width > (canvas.width - 20)) {
+      context.fontsize = context.fontsize - 2;
+      context.lineWidth = Math.max.apply(null, [context.lineWidth - 0.2, 2]);
+      context.font = "bold "+context.fontsize+"px Impact, sans-serif";
+    }
+  },
+
+  drawMultiLineTopText: function (lines) {
+    var that = this;
+    var startHeight = 50;
+    var line = this.longestLine(lines);
+    var context = this.$context;
+    var canvas = this.$canvas;
+    
+    this.configureTextSize(line);
+
+
+    lines.forEach( function (line, index) {
+      var tempsize = startHeight+index*context.fontsize*0.90;
+      context.fillText(line, that.center_x, tempsize);
+      context.strokeText(line, that.center_x, tempsize);
+    })
+  },
+
+  drawSingleLineText: function (text, xpos, ypos) {
   	var context = this.$context;
   	var canvas = this.$canvas;
     var startText = text.slice(0,30);
-    context.fontsize = 90;
-    context.font = "bold "+context.fontsize+"px Impact, sans-serif";
-    while (context.measureText(startText).width > (canvas.width-20)) {
-      ypos = ypos - 2;
-      console.log(console.fontsize);
-      context.fontsize = context.fontsize - 4;
-      context.lineWidth = context.lineWidth - 0.1;
-      context.font = "bold "+context.fontsize+"px Impact, sans-serif";
-    }
+
+    this.configureTextSize(startText);
 
     context.fillText(startText, xpos, ypos);
     context.strokeText(startText, xpos, ypos);
+    return context.fontsize;
   },
 
-  drawTopText: function (text, xpos, ypos) {
-    this.drawText(text.slice(0,30), xpos, ypos);
-    if (text.slice(30) !== "") {
-      this.drawTopText(text.slice(30), xpos, ypos+40);
-    }
+  drawSingleLineTopText: function (text, xpos) {
+    this.drawSingleLineText(text.slice(0,30), xpos, 100);
   },
 
-  drawBottomText: function (text, xpos) {
+  drawSingleLineBottomText: function (text, xpos) {
   	var canvas = this.$canvas;
     var lines = Math.ceil(text.length/30);
     var ypos = canvas.height - lines*30;
     for (var i=0; i < lines; i++) {
-      this.drawText(text.slice(i*30, (i+1)*30), xpos, ypos);
+      this.drawSingleLineText(text.slice(i*30, (i+1)*30), xpos, ypos);
       ypos += 40;
     }
-  }
+  },
+
+  wordSplit: function (str) {
+    var that = this;
+    words = str.split(" ");
+    var newWords = [];
+
+    words.forEach( function (word) {
+      if (word.length >= that.maxLineLength) {
+        var chunker = new RegExp('.\{1,'+that.maxLineLength+'\}', 'g');
+        var chunks = word.match(chunker);
+        newWords = newWords.concat(chunks);
+      } else {
+        newWords.push(word);
+      }
+    })
+
+    return newWords;
+  },
+
+  buildLines: function (str) {
+    var words = this.wordSplit(str);
+    var maxLineLength = this.maxLineLength;
+    var lines = [];
+    var currentLine = [];
+
+    while (words.length !== 0) {
+      console.log('loop')
+      console.log(words[0].length);
+      var newstr = currentLine.concat(words[0]).join(" ");
+      
+      if (newstr.length > maxLineLength) {
+        lines.push(currentLine.join(" "));
+        currentLine = [];
+      } else {
+        currentLine.push( words.shift() );
+      }
+    }
+    lines.push(currentLine.join(" "));
+
+    return lines;
+  },
+
+  longestLine: function (lines) {
+     var longest = lines[0];
+     for (var i = 0; i < lines.length; i++) {
+        if (longest.length < lines[i].length) {
+          longest = lines[i];
+        }
+     }
+     return longest;
+  },
+
 
 });
