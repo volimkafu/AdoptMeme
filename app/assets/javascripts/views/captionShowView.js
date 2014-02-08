b6eae56753d06847bcf0f154b6cd7a426a38b0af
Jen Hamon <jhamon@gmail.com>
2014-02-07 17:56:12 -0800

    Debug caption show.

--- app/assets/javascripts/views/captionShowView.js
+++ app/assets/javascripts/views/captionShowView.js
@@ -5,24 +5,24 @@ AdoptMeme.Views.captionShowView = Backbone.View.extend({
 
   initialize: function (options) {
     this.captionid = options.captionid;
-    this.listenTo(this.collection, "change sync", this.render);
+    this.listenTo(this.model, "change sync", this.render);
   },
 
   render: function () {
     var innerContainer = $("<div class='inner-container'>");
-  	var caption = this.collection.get(this.captionid);
+    var caption = this.model;
 
     if (caption) {
       var renderedContent = $(this.template({ caption: caption.attributes }));
-      var pet = AdoptMeme.pets.get({id: caption.attributes.pet_id })
+      var pet = AdoptMeme.pets.get({id: caption.attributes.pet_id });
+      // var pet = new AdoptMeme.Models.pet({id: caption.attributes.pet_id});
       pet.set({"image_id": caption.attributes.image_id});
       var petAdoptionInfoView = new AdoptMeme.Views.petAdoptionInfo({model: pet});
-      debugger;
       innerContainer.append(renderedContent);
       innerContainer.append(petAdoptionInfoView.render().$el);
     }
-    console.log(innerContainer)
+
     this.$el.html(innerContainer);
-		return this
+		return this;
   }
-})
+});
